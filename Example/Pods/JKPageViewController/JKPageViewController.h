52e20c8b7134ea4085bb679676b3b7ef65670896
kimjune01 <kimjune01@gmail.com>
2015-03-03 13:51:56 -0800

    removed sample

---
+++ Example/Pods/JKPageViewController/JKPageViewController.h
@@ -0,0 +1,19 @@
+//
+//  JKPageViewController.h
+//  JKPageViewController
+//
+//  Created by Camvy Films on 2015-03-03.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JKPageViewController.
+FOUNDATION_EXPORT double JKPageViewControllerVersionNumber;
+
+//! Project version string for JKPageViewController.
+FOUNDATION_EXPORT const unsigned char JKPageViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JKPageViewController/PublicHeader.h>
+
+
